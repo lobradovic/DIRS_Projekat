b606ee40a2233a4888f1cc6b9b920cf08022a8de
Luka Obradovic <lukaobradovic5@gmail.com>
2026-05-20 02:41:44 +0200

    created models and initial database

---
+++ DIRS_Ketering/Models/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIRS_Ketering.Models
+{
+    public enum Status
+    {
+        Narucena,
+        Izvrsena,
+        Otkazana
+    }
+}
---
+++ DIRS_Ketering/Models/Stavka.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIRS_Ketering.Models
+{
+    public class Stavka
+    {
+        public int Id { get; set; }
+        public int Kolicina { get; set; }
+        public decimal TrenutnaCena { get; set; }
+        public int PorudzbineId { get; set; }
+        public int JeloId { get; set; }
+
+        public Porudzbina Porudzbina { get; set; } = null!;
+        public Jelo Jelo { get; set; } = null!;
+    }
+}
